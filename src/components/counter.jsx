2f503065c832fd9a7f19b8f9ec16407f4cb3a0a2
Maximilian <scientificresearch309@gmail.com>
2022-11-01 12:46:15 +0100

    Counter has done.

---
+++ src/components/counter.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 70,
+    tags: ["tag1", "tag2", "tag3", "tags4", "tag5", "tag6"],
+    // tags: [],
+  };
+  // constructor(_a, _b, _c) {
+  //   super();
+  //   this.a = _a; //Z.Beispiele
+  //   this.b = _b; //Z.Beispiele
+  //   this.c = _c; //Z.Beispiele
+  //   // console.log("Constructor", this);
+  //   // Weil es eine onClick function ist, benutzen wir  .bind() in einem  Konstruktor, um sie mit .this weiter benutzen zu können,
+  //   // und wir werden nicht handleIncrement function as Parameter in  runde Klamme wie _a,_b,_c schreiben.
+  //   // mit normaler Funktion wie renderTags() unten, brauchen wir keiner Konstruktor, um .this zu zugreifen.
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  //In Eventhandler(onClick) haben wir keiner Zugriff zur .this
+
+  // Zweite Lösung zum Zugriff .this Ohne Konstruktor zu nutzen ist: Die Verwendung eine Arrow function(Pfeil Funktion)
+  handleIncrement = (nameVonProdukt) => {
+    let result = this.state.count++;
+    console.log("Increment Clicked!", result);
+    console.log("Name des Produkts: " + nameVonProdukt);
+
+    this.setState({ count: this.state.count++ });
+    // console.log(this.state.tags);
+    // return result;
+  };
+
+  doHandleIncrement = () => {
+    const a = {
+      id: 100,
+      name: "Poluver H&M",
+    };
+    this.handleIncrement(a.name);
+  };
+
+  renderTags() {
+    console.log(this.state.tags.length);
+    // Ternary Operators
+    // return this.state.tags.length === 0 ? (
+    //   <p>"There are no tags"</p>
+    // ) : (
+    //   <ul>
+    //     {this.state.tags.map((tag) => (
+    //       <li key={tag}>{tag}</li>
+    //     ))}
+    //   </ul>
+    // );
+    // oder mit if-Anweisungen
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  render() {
+    return (
+      // <div>
+      //   {this.state.tags.length === 0 && "There are no Tags"}
+      //   {this.renderTags()}
+      // </div>
+      (<p>{this.renderTags()}</p>),
+      (
+        <div>
+          <span className={this.getBadgeClasses()}>{this.format()}</span>
+          <button
+            onClick={this.doHandleIncrement}
+            className="btn btn-secondary btn-sm"
+          >
+            Increment
+          </button>
+        </div>
+      )
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  format() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+export default Counter;
